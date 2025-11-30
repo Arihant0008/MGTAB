6abf9a880c149e5e415dc87be9509a1861fc938c
“Arihant0008” <jain.arihant0008@gmail.com>
2025-12-01 01:46:18 +0530

    Added changes to index.html and app.py

--- constructed_graph/model_comparision.py
+++ constructed_graph/model_comparision.py
@@ -2,11 +2,13 @@ import pandas as pd
 
 data = {
     "Model": ["GraphSAGE","R-GCN","GCN","GAT"],
-    "Best Val Acc": [0.89,0.89,0.80,0.86],
-    "Test Acc": [0.89,0.91,0.83,0.88],
+    "Best Val Acc": [0.86,0.87,0.80,0.86],
+    "Test Acc": [0.89,0.90,0.83,0.88],
     "Model File": ["best_sage_model.pt","best_rgcn_model.pt","best_gcn.pt","best_gat.pt"]
 }
-
+# Arihant bhau?
+# ha ruk
+# neend a
 df = pd.DataFrame(data)
 df.to_csv("model_comparison.csv", index=False)
 print(df)
